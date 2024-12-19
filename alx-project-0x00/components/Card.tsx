e0ad72f522823c35fb4dc7cddf35940e2245ee17
Miftah <miftahaadem@gmail.com>
2024-12-19 22:45:17 +0300

    Using components

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import HOUSE_IMAGE from '@/public/assets/house.png'
+import HOUSE_IMAGE from '/public/assets/house.png'
 import STAR_IMAGE from '@/public/assets/star.png'
 import React from 'react'
 import Pill from './Pill'
